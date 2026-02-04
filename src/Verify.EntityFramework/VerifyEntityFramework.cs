95e2a240cac9f99e52952b399b2aaff6b28b9a51
Simon Cropp <simon.cropp@gmail.com>
2026-02-04 21:59:55 +1100

    Update VerifyEntityFramework.cs

--- src/Verify.EntityFramework/VerifyEntityFramework.cs
+++ src/Verify.EntityFramework/VerifyEntityFramework.cs
@@ -146,7 +146,7 @@ public static class VerifyEntityFramework
         var queryable = (IQueryable) arg;
 
         var sql = queryable.ToQueryString();
-        if (formatSql)
+        if (queryable.ShouldFormatSql())
         {
             sql = SqlFormatter.Format(sql).ToString();
         }
